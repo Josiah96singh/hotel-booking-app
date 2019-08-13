ff1782cc734a32c733393fab70957a40fdc4f07d
Josiah <Josiahsingh1996@gmail.com>
2019-08-13 22:52:57 +0300

    added context api and rendered sample data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import { RoomContext } from "../Context";
 import Loading from "./Loading";
 import Room from "./Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
     return (
-      <div>
-        from featured rooms
-        <Room />
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
