375d2a15db8aaa3a31e4ef58bb20348991769433
Josiah <Josiahsingh1996@gmail.com>
2019-09-24 17:36:51 +0300

    connected with contentful and rendering data

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import items from "./data";
+// import items from "./data";
+import Client from "./Contentful";
 
 const RoomContext = React.createContext();
 
@@ -21,21 +22,31 @@ class RoomProvider extends Component {
   };
 
   // getData ** TODO
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "beachResortRoom",
+        order: "fields.price"
+      });
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        maxPrice,
+        maxSize
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   componentDidMount() {
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-    let maxPrice = Math.max(...rooms.map(item => item.price));
-    let maxSize = Math.max(...rooms.map(item => item.size));
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      maxPrice,
-      maxSize
-    });
-    // console.log(featuredRooms);
+    this.getData();
   }
 
   formatData(items) {
@@ -57,7 +68,7 @@ class RoomProvider extends Component {
 
   handleChange = event => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
@@ -79,23 +90,40 @@ class RoomProvider extends Component {
       pets
     } = this.state;
 
-    // All the rooms
+    // all the rooms
     let tempRooms = [...rooms];
-    // Transform value
+    // transform value
     capacity = parseInt(capacity);
 
-    // Filter by type
+    // filter by type
     if (type !== "all") {
       tempRooms = tempRooms.filter(room => room.type === type);
     }
 
-    // Filter by capacity
+    // filter by capacity
     if (capacity !== 1) {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
 
     // filter by price
     tempRooms = tempRooms.filter(room => room.price <= price);
+
+    // filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+
+    // change state
     this.setState({
       sortedRooms: tempRooms
     });
